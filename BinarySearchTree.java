d0b5637c29ae0a33471760e37d2472a14d943b8a
PRAFUL KUMAR <praful.k180@gmail.com>
2018-05-29 22:26:04 +0530

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,147 +1,124 @@
 import java.util.Comparator;
 
 class BinarySearchTree<T> 
-{
-    private BSTNode root;
-    private Comparator <T>key;
-    private class BSTNode
-    {
-        T data;
-        BSTNode left,right;
-        long height;
-        BSTNode(T data)
-        {
-            this.data=data;
-            left=null;
-            right=null;
-        }
-    }
-    public BinarySearchTree(Comparator<T> key)
-    {
-        this.key=key;
-        root=null;
-    }
-
-    public boolean insert(T data)
-    {
-        BSTNode node=search(root,data);
-        if(node!=null && node.data.equals(data))//element already present
-            return false;
-        else
-        {
-            BSTNode newNode=new BSTNode(data);
-            if(node==null)
-                root=newNode;
-            else
-            {
-                int insertKey=key.compare(node.data, data);
-                if(insertKey>1)
-                {
-                    node.left=newNode;
-                }
-                else
-                    node.right=newNode;
-
-            }
-            return true;
-
-        }
-        //	root=insert(root,new BSTNode(data));
-    }
-
-    public boolean search(T data)
-    {
-        BSTNode node=search(root,data);
-        if(node==null)//bst empty
-            return false;
-        else if(node.data.equals(data))//element found
-            return true;
-        else
-            return false;//element not present
-    }
-
-    private BSTNode search(BSTNode root,T data)
-    {
-        if(root==null)
-            return null;
-        int searchKey=key.compare(root.data, data);
-        if(searchKey==0)
-            return root;
-        else if(searchKey>1)
-        {
-
-            System.out.println("left");
-            if(root.left==null)
-                return root;
-            else
-                return search(root.left,data);
-        }
-        else
-        {
-
-            System.out.println("right");
-            if(root.right==null)
-                return root;
-            else
-                return search(root.right,data);
-        }
-    }
-
-    boolean remove(T data)
-    {
-        //yet to complete
-        return false;
-    }
-
-    public void preOrder()
-    {
-
-        System.out.println("PreOrder");
-        preOrder(root);
-    }
-
-    private void preOrder(BinarySearchTree<T>.BSTNode root2)
-    {
-        if(root2==null)
-            return;
-        System.out.print(root2.data.toString()+" ");
-        preOrder(root2.left);
-        preOrder(root2.right);
-
-    }
-
-    public void inOrder()
-    {
-
-        System.out.println("inOrder");
-        inOrder(root);
-    }
-
-    private void inOrder(BinarySearchTree<T>.BSTNode root2)
-    {
-        // TODO Auto-generated method stub
-        if(root2==null)
-            return;
-        inOrder(root2.left);
-        System.out.print(root2.data.toString()+" ");
-        inOrder(root2.right);
-    }
-
-    public void postOrder()
-    {
-
-        System.out.println("PostOrder");
-        postOrder(root);
-    }
-
-    private void postOrder(BinarySearchTree<T>.BSTNode root2)
-    {
-        // TODO Auto-generated method stub
-        if(root2==null)
-            return;
-        postOrder(root2.left);
-        postOrder(root2.right);
-        System.out.print(root2.data.toString()+" ");
-
-    }
-}
+	{
+		private BSTNode root;
+		private Comparator <T>key;
+		private class BSTNode
+		{
+			T data;
+			BSTNode left,right;
+			@SuppressWarnings("unused")
+			int level;
+			BSTNode(T data)
+			{
+				this.data=data;
+				left=null;
+				right=null;
+				level=1;
+			}
+			public BSTNode getLeft()
+				{
+					return left;
+				}
+			public BSTNode getRight()
+				{
+					return right;
+				}
+			public int getLevel()
+				{
+					return level;
+				}
+			private void copy(BSTNode inorderPrecessor)
+				{
+					// TODO Auto-generated method stub
+					
+				}
+		
+		}
+		public BinarySearchTree(Comparator<T> key)
+			{
+				this.key=key;
+				root=null;
+			}
+		public void insert(T data)
+			{
+				root=insert(root,data);
+			}
+		private BSTNode insert(BSTNode root,T data)
+		{
+			if(root==null)
+				return new BSTNode(data);
+			else
+				{
+					if(key.compare(root.data, data)<0)
+						root.left=insert(root.left,data);
+					else
+						root.right=insert(root.right,data);
+					return root;
+				}
+		}
+		
+		public int getLevel(T data)
+			{
+				return search(root, data).getLevel();
+			}
+		
+		public BSTNode search(T data)
+		{
+			return search(root,data);
+		}
+		private BSTNode search(BSTNode root,T data)
+		{
+			int result=key.compare(root.data, data);
+			if(root==null||result==0)
+				return root;
+			else if(result<0)
+				return search(root.left,data);
+			else
+				return search(root.right,data);
+		}
+		public void remove(T data)
+		{
+			remove(root,data);
+		}
+		private BSTNode maxNode(BSTNode node)
+		{
+			while(node.right!=null)
+				node=node.right;
+			return node;
+		}
+		private BSTNode remove(BSTNode root,T data)
+			{
+				if(root==null)
+					return root;
+				int result=key.compare(root.data, data);
+				if(result<0)
+					{
+						root.left=remove(root.left, data);
+					}
+				else if(result>0)
+					{
+						root.right=remove(root.right, data);
+					}
+				else
+					{
+						if(root.left==null)
+							{
+								return root.right;
+							}
+						else if(root.right==null)
+							{
+								return root.left;
+							}
+						else
+							{
+								BSTNode inorderPrecessor=maxNode(root.left);
+								root.copy(inorderPrecessor);
+								root.left=remove(root.left, inorderPrecessor.data);
+							}
+					}
+				return root;
+			}
+	
+		}
